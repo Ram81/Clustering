d77a21fde622fae893b9bee6ddae4cb8206da249
Ram81 <ramramrakhya81@gmail.com>
2017-03-27 19:40:10 +0530

    Handwritten Digit Detection Done with K-means

--- classifier.py
+++ classifier.py
@@ -52,10 +52,46 @@ print('\n')
 
 print(79*'_')	
 	
+#Visualize Data	
+reduced_data = PCA(n_components = 2).fit_transform(data)
+
+kmeans = KMeans(init = "k-means++", n_clusters = n_digits, n_init = 10)
+kmeans.fit(reduced_data)
+
+#Step Size of mesh
+h = .02	
 	
+#plot decision boundary
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1	
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 	
-	
-	
+#Obtain Labels for each point in mesh
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])	
+
+Z = Z.reshape(xx.shape)
+
+#plotting data
+plt.figure(1)
+plt.clf()
+
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)	
+
+#plotting centroids	
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1], marker = 'x', s = 169, linewidths = 3, color = 'w', zorder = 10)	
+
+plt.title('K-means clustering on the digits dataset (PCA-reduced data)\nCentroids are marked with white cross')
+plt.xlim(x_min, x_max)	
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
 	
 	
 	
