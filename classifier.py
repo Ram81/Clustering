856ce4bd411f9dca181bb03f7668b2449814d6f9
Ram81 <ramramrakhya81@gmail.com>
2017-03-27 17:41:37 +0530

    classifier.oy added

---
+++ classifier.py
@@ -0,0 +1,11 @@
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+np.random.seed(42)
