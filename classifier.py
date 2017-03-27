ee9dc2dad2ebf447185a258cbba9e9a99371452c
Ram81 <ramramrakhya81@gmail.com>
2017-03-27 17:52:38 +0530

    Total Outputs identified

--- classifier.py
+++ classifier.py
@@ -10,8 +10,17 @@ from sklearn.preprocessing import scale
 
 np.random.seed(42)
 
+
 digits = load_digits()
 data = scale(digits.data)
 
+#loading size of dataset
 n_sample, n_feature = data.shape
 print(n_sample," ", n_feature)
+
+#obtaining total number of clusters based on the output i.e. target
+n_digits = len(np.unique(digits.target))
+labels = digits.target
+print(n_digits)
+
+samples = 300
