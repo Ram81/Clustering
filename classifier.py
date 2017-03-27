0534268e0d4339d7942e9510d39090a41d7cb6a2
Ram81 <ramramrakhya81@gmail.com>
2017-03-27 19:11:32 +0530

    Kmeans with random clusters and kmeans++ centroids

--- classifier.py
+++ classifier.py
@@ -16,11 +16,45 @@ data = scale(digits.data)
 
 #loading size of dataset
 n_sample, n_feature = data.shape
-print(n_sample," ", n_feature)
+#print(n_sample," ", n_feature)
 
 #obtaining total number of clusters based on the output i.e. target
 n_digits = len(np.unique(digits.target))
 labels = digits.target
-print(n_digits)
+#print(n_digits)
 
 samples = 300
+print("\n n_digits: %d, \t samples: %d, \t n_features: %d"%(n_digits, samples, n_feature))
+
+
+print(79*'_')
+
+print(' %9s '%'init      time inertia homo compl v-meas ARI AMI Silhouette\n')
+
+#here estimator is the model being fitted to te data or applied to the data
+def analyze_k_means(estimator, name, data):
+	t0 = time()
+	estimator.fit(data)	
+	print(" %9s %.2fs %i %.3f %.3f %.3f %.3f %.3f %.3f"%( name, time()-t0, estimator.inertia_, metrics.homogeneity_score(labels,  estimator.labels_), metrics.completeness_score(labels, estimator.labels_), metrics.v_measure_score(labels, estimator.labels_), metrics.adjusted_rand_score(labels, estimator.labels_), metrics.adjusted_mutual_info_score(labels, estimator.labels_), metrics.silhouette_score(data, estimator.labels_, metric='euclidean', sample_size = samples) ))
+	
+
+analyze_k_means( KMeans(init = 'k-means++', n_clusters = n_digits, n_init = 10), name="k-means++", data = data)	
+
+
+analyze_k_means( KMeans(init = 'random', n_clusters = n_digits, n_init = 10), name="random", data = data)
+
+print('\n')
+
+print(79*'_')	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
